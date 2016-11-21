7baba4f194dbf99b49056e2da6d3adf19df5f3e6
1408876 <a.skeiryte@rgu.ac.uk>
2016-11-21 15:52:42 +0000

    updating connect.js

--- connect.js
+++ connect.js
@@ -6,33 +6,33 @@ http.createServer(function(request, response) {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
     response.write('Connecting \n');
 
-var mongodb = require('mongodb');
-//and our HTTP server
-var http = require('http');
-//setup our port
-var port = process.env.PORT || 1337;
+    var mongodb = require('mongodb');
+    //and our HTTP server
+    var http = require('http');
+    //setup our port
+    var port = process.env.PORT || 1337;
 
-var url = 'mongodb://AgneSkeiryte:609dcc19a@ds050879.mlab.com:50879/nodedatabase';
+    var url = 'mongodb://AgneSkeiryte:609dcc19a@ds050879.mlab.com:50879/nodedatabase';
 
-var MongoClient = mongodb.MongoClient;
+    var MongoClient = mongodb.MongoClient;
 
-MongoClient.connect(url, function (err, db) {
-    response.write('Connection Made \n');
-    if (err) {
-        response.write('Unable to connect to the mongoDB server. Error:' + err + "\n");
-        //Error so close connection
-        db.close();
-    } else {
-        //HURRAY!! We are connected. :)
-        response.write('Connection established to' + url +"\n");
+    MongoClient.connect(url, function (err, db) {
+        response.write('Connection Made \n');
+        if (err) {
+            response.write('Unable to connect to the mongoDB server. Error:' + err + "\n");
+            //Error so close connection
+            db.close();
+        } else {
+            //HURRAY!! We are connected. :)
+            response.write('Connection established to' + url +"\n");
 
-        // do some work here with the database.
+            // do some work here with the database.
 
-        //Done Close connection
-        db.close();
-    }
-    response.end('Finished, Connection closed \n');
-});
+            //Done Close connection
+            db.close();
+        }
+        response.end('Finished, Connection closed \n');
+    });
 
 }).listen(port);
 
